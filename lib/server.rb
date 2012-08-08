eb5b771ffd9c0c8ed43f8f1a1b88b30265774f4a
Julien Portalier <julien@portalier.com>
2012-08-08 19:13:05 +0200

    Initial import: working proxy from TCP to UNIX sockets.

---
+++ lib/server.rb
@@ -0,0 +1,60 @@
+require "socket"
+#require "ap"
+
+server = TCPServer.new("localhost", 20559)
+
+loop {
+  Thread.start(server.accept) do |input|
+    output = UNIXSocket.new("/tmp/rack.sock")
+
+    # SOCKET > RACK SERVER
+    content_length = 0
+    while line = input.gets
+      line.set_encoding("ASCII-8BIT") if line.respond_to?(:set_encoding)
+
+      if line.strip =~ %r{^([A-Z]+) (.+) HTTP/1\.(\d+)$}
+        puts "[#{Time.now}] #{line.strip}"
+      end
+      if line.strip =~ /^Content-Length: (\d+)$/
+        content_length = $1.to_i
+      end
+
+      output.write(line)
+      break if line.strip.empty?
+    end
+
+    output.write(input.read(content_length)) if content_length > 0
+    output.flush
+
+    # RACK SERVER > SOCKET
+    content_length = 0
+    connection = nil
+    while line = output.gets
+      line.set_encoding("ASCII-8BIT") if line.respond_to?(:set_encoding)
+
+      if line.strip =~ /^Content-Length: (\d+)$/
+        content_length = $1.to_i
+      end
+      if line.strip =~ /^Connection: (.+)$/
+        connection = $1.strip
+      end
+
+      input.write(line)
+      break if line.strip.empty?
+    end
+
+    if content_length > 0
+      input.write(output.read(content_length)) 
+    elsif connection == "close"
+      begin
+        input.write(output.read)
+      rescue EOFError
+      end
+    end
+    input.flush
+
+    input.close
+    output.close
+  end
+}
+
