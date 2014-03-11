39fac3906dc03a5bc435f635e94a79dafede83c2
Karl Wilbur <karl@kandrsoftware.com>
2014-03-11 15:44:21 -0400

    Don't create an encrypted key (password protected) for local development.

--- ssl/generate.sh
+++ ssl/generate.sh
@@ -1,7 +1,4 @@
 #! /usr/bin/env sh
 
-openssl genrsa -out server.orig.key 2048
-openssl rsa -in server.orig.key -out server.key
-openssl req -new -key server.key -out server.csr
+openssl req -new -newkey rsa:2048 -nodes -keyout server.key -out server.csr
 openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
-
