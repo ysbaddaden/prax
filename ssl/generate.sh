b6f41309b3382e331b41b890069f12893cbb3dce
Karl Wilbur <karl@kandrsoftware.com>
2014-03-17 06:05:29 -0400

    Added SSL key and cert generation to install.

--- ssl/generate.sh
+++ ssl/generate.sh
@@ -1,4 +1,28 @@
 #! /usr/bin/env sh
 
-openssl req -new -newkey rsa:2048 -nodes -keyout server.key -out server.csr
-openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+keygen() {
+  openssl genrsa -out server.key 2048
+}
+csrgen() {
+  openssl req -new -key server.key -out server.csr -subj "/C=/ST=/L=/O=Prax Dev Cert/OU=/CN=localhost"
+}
+crtgen() {
+  openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+}
+
+case "$1" in
+  keygen)
+    keygen
+    ;;
+  csrgen)
+    csrgen
+    ;;
+  crtgen)
+    crtgen
+    ;;
+  *)
+    keygen
+    csrgen
+    crtgen
+    ;;
+esac
