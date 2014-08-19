375e2b534556cbcf66652ea778cfb0b4809d0d2c
Thomas Kriechbaumer <kriechbaumer@gmail.com>
2014-08-19 15:05:22 +0200

    use double brackets for bash conditionals

--- libexec/shared.sh
+++ libexec/shared.sh
@@ -1,6 +1,6 @@
 ensure_rack_app() {
-  if [ ! -f "$1/config.ru" ]; then
-    if [ ! -d "$1/public" ]; then
+  if [[ ! -f "$1/config.ru" ]]; then
+    if [[ ! -d "$1/public" ]]; then
       echo "Error: this does not appear to be a rack app (config.ru is"
       echo "missing) and there isn't any public/ folder either."
       exit
