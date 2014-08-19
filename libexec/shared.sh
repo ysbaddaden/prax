2f98f6e6d806b07cbf1e80fa961203fc354ec323
Thomas Kriechbaumer <kriechbaumer@gmail.com>
2014-08-19 15:08:09 +0200

    normalise indentation

--- libexec/shared.sh
+++ libexec/shared.sh
@@ -1,9 +1,9 @@
 ensure_rack_app() {
-    if [ ! -f "$1/config.ru" ]; then
-        if [ ! -d "$1/public" ]; then
-            echo "Error: this does not appear to be a rack app (config.ru is"
-            echo "missing) and there isn't any public/ folder either."
-            exit
-        fi
+  if [ ! -f "$1/config.ru" ]; then
+    if [ ! -d "$1/public" ]; then
+      echo "Error: this does not appear to be a rack app (config.ru is"
+      echo "missing) and there isn't any public/ folder either."
+      exit
     fi
+  fi
 }
