0b49eca61f064166103edd3d5899f8769432d9d0
Julien Portalier <julien@portalier.com>
2014-11-25 12:01:35 +0100

    Fix: SSL key generation may need root permissions

--- ssl/generate.sh
+++ ssl/generate.sh
@@ -1,14 +1,16 @@
 #! /usr/bin/env sh
 
+SUDO=`touch "test" 2>/dev/null && rm "test" || echo "sudo"`
+
 if [ ! -e server.key ]; then
-  openssl genrsa -out server.key 2048
+  $SUDO openssl genrsa -out server.key 2048
 fi
 
 if [ ! -e server.csr ]; then
-  openssl req -new -key server.key -out server.csr \
+  $SUDO openssl req -new -key server.key -out server.csr \
     -subj "/C=/ST=/L=/O=Prax Dev Cert/OU=/CN=localhost"
 fi
 
 if [ ! -e server.crt ]; then
-  openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
+  $SUDO openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt
 fi
